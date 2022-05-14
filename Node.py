7b86d949c9aa35f9e9f5c862c588ea4fb67d4d3b
Sergei Zinchenko <58306690+mordiggian174@users.noreply.github.com>
2022-05-15 00:20:37 +0700

    Create Node.py

---
+++ Node.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.parent = None
+        self.rightChild = None
+        self.leftChild = None         
+        self.height = 0
+        self.size = 1
+    def __str__(self):
+        return str(self.key)#+'('+str(self.height)+')'
+    def is_leaf(self):
+        return self.height == 0
+    def max_children_height(self):
+        if self.leftChild and self.rightChild:
+            return max(self.leftChild.height,self.rightChild.height)
+        elif self.leftChild:
+            return self.leftChild.height
+        elif self.rightChild:
+            return self.rightChild.height
+        else:
+            return -1
+
+    def balance(self):
+        return (self.leftChild.height if self.leftChild else -1) -\
+               (self.rightChild.height if self.rightChild else -1)
